4e1cac3da60ec73c0168af277e6ae145546e9be1
Rishabh Dasgupta <rishabhdasu@gmail.com>
2022-02-15 02:48:20 +0530

    add

---
+++ block-BJaaim/code/index.js
@@ -0,0 +1,27 @@
+class Question {
+    constructor(title, correctAnswer) {
+        this.title = title;
+        this.options = [];
+        this.correctAnswer = answer;
+    }
+    isCorrect(answer) {
+        return this.answer === answer;
+    }
+    getCorrectAnswer() {
+        return `Correct answer is ${correctAnswer}`
+    }
+    createUI() {
+        this.
+    }
+}
+
+class Quiz {
+    constructor(allQuestions, activeQuestion, scoreTotal) {
+        this.allQuestions = [];
+        this.activeQuestion = activeQuestion;
+        this.scoreTotal = scoreTotal;
+    }
+    nextQuestion() {
+        
+    }
+}
