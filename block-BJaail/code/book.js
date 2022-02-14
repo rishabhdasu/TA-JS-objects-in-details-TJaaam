79d87c4134590217a203015795ab834a0c73f073
Rishabh Dasgupta <rishabhdasu@gmail.com>
2022-02-15 02:38:31 +0530

    add

--- block-BJaail/code/book.js
+++ block-BJaail/code/book.js
@@ -1,52 +1,80 @@
-    class BookList {
-    constructor(root, list = []) {
-        this.books = list;
-        this.root = root;
-        return this.books.length;
-    }
-    add(name, author, isbn) {
-        let book = new Books(name, author, isbn);
-        this.books.push(book);
-        this.createUI();
-        return this.books.length;
-    }
-    handleDelete(id) {
-        let index = this.books.findIndex((book) => book.id = id);
-        this.books.splice(index, 1);
-        this.createUI();
-        return this.books.length;
-    }
-    createUI() {
-        this.books.forEach((book) => {
-            let ui = book.createUI();
-            ui.querySelector(".deleteIcon").addEventListener(`click`,
-             this.handleDelete.bind(this, book.id));
-            })
-    }
-}
+let form = document.querySelector(".form");
+let root = document.querySelector(".book_list");
 
-class Books {
-    constructor(name,author) {
+let nameElm = form.elements.bookName;
+let authorElm = form.elements.author;
+let imgElm = form.elements.img;
+
+class Book {
+    constructor(name, author, img) {
         this.name = name;
         this.author = author;
-        this.id = `id-${Date.now()}`;
-        this.isRead= false;
+        this.img = img;
+        this.isRead = false;
     }
-    createUI() {
-        let root = document.querySelector(".root");
-        let bookName = document.createElement("li");
-        bookName.classList.add("bookName");
-        bookName.innerText = this.name;
-        let author = document.createElement("li");
-        author.innerText = this.author;
-        let isbn = document.createElement("li");
-        isbn.innerText = this.author;
-        let deleteIcon = document.createElement("li")
-        deleteIcon.innerText = "❌";
-        root.append(bookName, author, isbn, deleteIcon);
-        return root;
+    togggleIsRead() {
+        this.isRead = !this.isRead;
     }
 }
 
-let firstBook = new BookList("Shout");
-firstBook.add("ad", "asas", "");
+class BookList {
+    constructor(books = []) {
+        this.books = books;
+    }
+    addBook(name, author, img) {
+        let book = new Book(name, author, img);
+        this.books.push(book);
+        this.createUI();
+    }
+    // handledelete(event) {
+
+    // }
+    createUI() {
+        root.innerHTML = '';
+        this.books.forEach((book, index) => {
+            let img = document.createElement("img");
+            img.classList.add("full")
+            img.src = book.img;
+            let li = document.createElement("li");
+            li.classList.add("flex-33")
+            let h2 = document.createElement("h2");
+            h2.innerText = book.name;
+            let p = document.createElement("p");
+            p.innerText = book.author;
+            let button = document.createElement("button");
+            button.classList.add("btn");
+            button.innerText = book.isRead ? "Completed" :  "Mark as Read";
+            button.addEventListener(`click`, ()=> {
+                book.togggleIsRead();
+                this.createUI();
+            })
+            let remove = document.createElement("button");
+            remove.classList.add("remove-btn");
+            remove.innerText = "Remove Book";
+            remove.addEventListener(`click`, (event) => {
+                let id = event.target.dataset.id;
+                this.books.splice(id, 1);
+                this.createUI();
+            });
+            remove.setAttribute("data-id", index)
+            li.append(img, h2, p,  button, remove);
+            root.append(li);
+        })
+    }
+}
+
+let library = new BookList();
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let name = nameElm.value;
+    let author = authorElm.value;
+    let img = imgElm.value;
+    library.addBook(name, author, img);
+    nameElm.value = ``;
+    authorElm.value = ``;
+    imgElm.value = ``;
+}
+
+form.addEventListener(`submit`, handleSubmit);
